6b06d05fc71c5aed5a1b0b31cb81c03cf3748a55
regnisalram <regnisalram@gmail.com>
2016-06-05 16:54:15 +0200

    #2 #3 #4
    - get content from json file
    - read json file once
    - path for json file

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 # from .models import Message
+import json
+from config.settings.base import DJANGO_ROOT
 
+
+with open(DJANGO_ROOT + '/data/messages.json') as f:
+    json_str = f.read()
+
+data = json.loads(json_str)
 
 def home(request):
     # messages = Message.objects.order_by('order')
-    messages = ['bla', 'blabla', 'blablabla']
     context_dict = {
-        'messages': messages
+    	'messages': data
     }
+
     return render(request, 'starter_app/home.html', context_dict)
